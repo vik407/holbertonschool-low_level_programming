4b562794e15ec8bf46bee60262b92bc3cd8f2462
Victor Hernandez <vik407@gmail.com>
2019-09-18 21:45:15 +0000

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string using printf
+ *
+ * Return: with proper grammar, but the outcome is a piece of art
+ */
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
