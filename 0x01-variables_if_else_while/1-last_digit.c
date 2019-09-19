eecd554cad5efdac404637340b5601e1b29bfd1c
Victor Hernandez <vik407@gmail.com>
2019-09-19 20:44:13 +0000

    Last digit a wrong string

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - print the last digit of the number stored in the variable n
+ * main - prints out a random number
+ * and check the last digit is greater than 5
+ * or less than 6 but not 0
  * Return: 0
  */
 
@@ -22,7 +24,7 @@ int main(void)
 	else if (s == 0)
 		printf("Last digit of %d is %d and is 0\n", n, s);
 	else
-		printf("Last digit of %d is %d and is less than 5 and not 0\n", n, s);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
 
 	return (0);
 }
