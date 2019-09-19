31dee838ffd9ab806eaf041cf8d1ab22b54dd9e8
Victor Hernandez <vik407@gmail.com>
2019-09-19 01:44:31 +0000

    Finally

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,12 @@
  * Return: with proper grammar, but the outcome is a piece of art
  */
 
-int main(void)
+main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
