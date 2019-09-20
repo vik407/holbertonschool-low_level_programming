92a3756aa52ad9421ca4671d2402d706a893cd3e
Victor Hernandez <vik407@gmail.com>
2019-09-20 03:46:37 +0000

    Print comb 2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 00 to 99.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, j, k;
+
+	for (a = 0; a < 100; a++)
+	{
+		j = a / 10;
+		k = a % 10;
+
+		putchar(j + '0');
+		putchar(k + '0');
+
+		if (a < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
