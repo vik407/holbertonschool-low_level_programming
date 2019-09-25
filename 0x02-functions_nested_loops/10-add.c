02a30544db5cf4455b38ea3d3f7019171f44111b
Victor Hernandez <vik407@gmail.com>
2019-09-25 01:20:20 +0000

    add - simple as that

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - Entry point.
+ *
+ * @a: Integer one
+ * @b: Integer two
+ * Description: adds two integers and returns the result.
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
