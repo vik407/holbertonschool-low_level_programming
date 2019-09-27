a9ae3b1df066e9a4a2f8c1c69798b4d75ee144f2
Victor Hernandez <vik407@gmail.com>
2019-09-26 22:43:45 -0500

    fix trailing again

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
-
 /**
  * print_diagonal - draws a diagonal line on the terminal.
  * @n: is the number of times the character \ should be printed
  * Return: \.
  */
-
 void print_diagonal(int n)
 {
-	int a = 0, b = 0;
+	int a = 0;
+	int b = 0;
 
 	while (b < n)
 	{
