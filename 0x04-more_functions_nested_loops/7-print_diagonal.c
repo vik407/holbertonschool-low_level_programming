f37c5aca688741fec6bff3bf238a08b9584e81fc
Victor Hernandez <vik407@gmail.com>
2019-09-26 22:42:03 -0500

    fix putchar trailing slash

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal.
+ * @n: is the number of times the character \ should be printed
+ * Return: \.
+ */
+
+void print_diagonal(int n)
+{
+	int a = 0, b = 0;
+
+	while (b < n)
+	{
+		while (a < b)
+		{
+			_putchar(' ');
+			a++;
+		}	
+		a = 0;
+		_putchar(92);
+		_putchar('\n');
+		b++;
+	}
+
+	if (n <= 0)
+		_putchar('\n');
+}
