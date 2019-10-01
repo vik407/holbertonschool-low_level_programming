55c9102ececb8f5f1446c10492d355309892c927
Victor Hernandez <vik407@gmail.com>
2019-10-01 22:13:52 +0000

    Sum and rest to print backwards

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string, in reverse,
+ * followed by a new line
+ *
+ * @s: The String
+ * Return: string in reverse
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+
+	while (length >= 0)
+	{
+		_putchar(*(s + length));
+		length--;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c~
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ *
+ * @str: The String
+ * Return: string
+ */
+
+void _puts(char *str)
+{
+	int length = 0;
+
+	while (*(str + length) != '\0')
+	{
+		_putchar(*(str + length));
+		length++;
+	}
+	_putchar('\n');
+}
