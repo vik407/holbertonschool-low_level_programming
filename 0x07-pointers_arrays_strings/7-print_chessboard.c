efeeba565afd877a92b3886cf78e1b8cf6a68745
Victor Hernandez <vik407@gmail.com>
2019-10-08 04:05:15 +0000

    The chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - function that print a board.
+ *
+ * @a: pointer array to chess characters
+ * Return: a game.
+ **/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
