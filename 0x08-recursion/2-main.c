74132eb6fdd93666c81305d6e73c54b57a3b3ee6
Victor Hernandez <vik407@gmail.com>
2019-10-09 19:14:49 +0000

    setup the day

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
