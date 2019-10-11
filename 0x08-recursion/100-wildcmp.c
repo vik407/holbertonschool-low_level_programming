05981b891dcf652af9a8336ffbfb24f4870a493f
Victor Hernandez <vik407@gmail.com>
2019-10-10 23:23:03 -0500

    fix conditional error for the error in the checker

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -14,9 +14,9 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
-	if (*s2 == '*' && *(s1 + 1) != '\0' && *s2 == '\0')
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	if (*s1 == '\0' && *(s2 + 1) != '\0' && *s2 == '*')
 		return (0);
 	return (0);
 }
