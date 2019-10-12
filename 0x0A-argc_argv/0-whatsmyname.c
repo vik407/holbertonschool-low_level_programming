993acb677677873294cc51bb7e1e65f7a154b2d5
Victor Hernandez <vik407@gmail.com>
2019-10-12 21:42:56 +0000

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ *
+ * @argc: The number of arguments
+ * @argv: Any containig arguments
+ *
+ * Return: 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
