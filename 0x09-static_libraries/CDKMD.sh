86cf3e07aaa3653a9d5e8e80d0221a514612833a
Victor Hernandez <vik407@gmail.com>
2019-10-13 09:24:55 -0500

    Fixing 0x09-static_libraries

--- 0x09-static_libraries/CDKMD.sh
+++ 0x09-static_libraries/CDKMD.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat list|cut -d "." -f1|
-    while read CD_KND
-    do
+	while read CD_KND
+	do
 	Filename=$(find ../ -name "${CD_KND}.c" | head -n1)
 	echo "creating ${CD_KND} file"
 	gcc -g -O -c "${Filename}" -o "${CD_KND}.o"
--- 0x09-static_libraries/copy_c_files.sh
+++ 0x09-static_libraries/copy_c_files.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-gcc -c *.c
-ar rc liball.a *.o
+cat list|cut -d "." -f1|
+	while read CD_KND
+	do
+	Filename=$(find ../ -name "${CD_KND}.c" | head -n1)
+	echo "copying ${CD_KND} file"
+	cat "${Filename}" > "${CD_KND}.c"
+	done
