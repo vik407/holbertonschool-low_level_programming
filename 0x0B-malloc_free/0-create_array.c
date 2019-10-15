059f710de574d00816bf83152e687582f46882cd
Victor Hernandez <vik407@gmail.com>
2019-10-15 20:47:43 +0000

    Simple array with validations fix 0x2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,17 +16,18 @@ char *create_array(unsigned int size, char c)
 	char *res;
 	unsigned int i;
 
+	if (size <= 0)
+		return (NULL);
+
 	res = malloc(size * (sizeof(char)));
 
-	if (size <= 0)
+	if (res == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
 		res[i] = c;
 	}
-	if (res == NULL)
-		return (NULL);
 
 	return (res);
 }
