1090554ab41e5ec28f29855a20989d302a82cdd2
Victor Hernandez <vik407@gmail.com>
2019-10-21 15:54:01 +0000

    Django 0x0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - stores dog information
+ * @name: dog name
+ * @age:  dog age
+ * @owner: dog's owner
+ *
+ */
+
+struct dog
+{
+	/* data */
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
