9c4d17fbddc372ac425d011571a85bfe103c95c3
Victor Hernandez <vik407@gmail.com>
2019-10-21 21:22:37 -0500

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *res;
+	res = __FILE__;
+
+	printf("%s\n", res);
+	return (0);
+}
