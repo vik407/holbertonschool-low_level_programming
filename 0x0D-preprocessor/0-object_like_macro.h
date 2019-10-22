84034583bf094d8f31afecf178c4839a890b130f
Victor Hernandez <vik407@gmail.com>
2019-10-21 21:09:49 -0500

    0. Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef _OBJECT_LIKE_MACRO_H_
+#define _OBJECT_LIKE_MACRO_H_
+
+/**
+ * OBJECT_LIKE_MACRO - file that defines a macro named SIZE
+ * as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
