2cf5aeca70be10edd59473781d39a4ea255278b3
Victor Hernandez <vik407@gmail.com>
2019-10-23 17:50:39 -0500

    finally (if braces in c... :-( )

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func -  function that searches for an integer.
+ * @s: is the operator passed as argument to the program
+ *
+ * Return: This function returns a pointer to the function that corresponds
+ * to the operator given as a parameter. Example: get_op_func("+") should
+ * return a pointer to the function op_add
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*s == *ops[i].op)
+		{
+			return ((ops[i].f));
+		}
+		i++;
+	}
+
+	return (NULL);
+}
