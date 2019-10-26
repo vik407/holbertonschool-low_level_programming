2b251a134c5a3c6de46db1b44229fdc271d2f72d
Victor Hernandez <vik407@gmail.com>
2019-10-26 10:17:10 -0500

    3. To be is a to be the value of a variable 0x1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,11 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct any_format - structure to receive any format
+ * @any_format: the specified format
+ * @p: the function pointer
+ */
 typedef struct any_format
 {
 	char *any_format;
