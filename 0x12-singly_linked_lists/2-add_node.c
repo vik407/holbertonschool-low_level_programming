1c4b9f2a852716f415e6a7015418b1a765b43f61
Victor Hernandez <vik407@gmail.com>
2019-10-31 21:26:42 -0500

    2. Add node 0x1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,12 +5,15 @@
 /**
  * add_node - function that adds a new node at the beginning of a list_t list.
  * @str: head of the linked list
- * If str is NULL, print [0] (nil)
  * Return:  the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *value;
+	int lenstr;
+
+	for (lenstr = 0; str[lenstr] != '\0'; lenstr++)
+		;
 
 	/* Malloc here? */
 	value = malloc(sizeof(list_t));
@@ -18,17 +21,16 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		/* if value is NULL return NULL */
 		return (NULL);
+		free(value);
 	} else
 	{
-		/* Len del string entonces debo iterar por str y retornar el len y lo pongo en value-> len */
-		/* Add a new node to the top */
-		value->len = strlen(str);
-		/* Usar strdup(str)  y lo ponemos en value -> str*/
+		/* Add a new node */
+		value->len = lenstr;
 		value->str = strdup(str);
 		value->next = *head;
 		*head = value;
 	}
-	
 	/* return pointer */
 	return (value);
+	free(value);
 }
