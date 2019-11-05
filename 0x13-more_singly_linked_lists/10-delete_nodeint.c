c8070cd435d5fa405e382f4bf8977a175cae5514
Victor Hernandez <vik407@gmail.com>
2019-11-05 17:14:27 -0500

    10. Delete at index 0x0

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Write a function that inserts a new node at a
+ * given position.
+ * @head: head of the linked list
+ * @index: index where the new node should be added. Index starts at 0
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *ptr;
+	listint_t *result  = *head;
+	unsigned int i = 0;
+
+	/* if the linked list is empty -1 */
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	ptr = *head;
+	/* if the index is null */
+	if (!index)
+	{
+		result = result->next;
+		free(*head);
+		/* The new head is the result (tricky AF) */
+		*head = result;
+		/* Success Return 1 */
+		return (1);
+	}
+
+	/* Move the list */
+	while (i < index - 1)
+	{
+		/* -1 if the result value coming NULL */
+		if (result == NULL)
+		{
+			return (-1);
+		}
+		/* Move it */
+		result = result->next;
+		i++;
+	}
+	/* Return the result */
+	ptr = result->next;
+	result->next = ptr->next;
+	free(ptr);
+	return (1);
+}
