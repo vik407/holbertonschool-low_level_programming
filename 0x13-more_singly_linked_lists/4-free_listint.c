bcc397bc3008d55914fb85d9206303b516cb6a7b
Victor Hernandez <vik407@gmail.com>
2019-11-05 10:07:34 -0500

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t list.
+ * @head: head of the linked list
+ * Return: the free list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *result;
+
+	/* free nodes while head is not null */
+	while(head != NULL)
+	{
+		result = head;
+		/* Move the list */
+		head = head->next;
+		free(result);
+	}
+
+}
