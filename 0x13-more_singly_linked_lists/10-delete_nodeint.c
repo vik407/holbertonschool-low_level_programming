1c6df06a22d2b4f7dd72e7ea3f1ba1975270f03e
Victor Hernandez <vik407@gmail.com>
2019-11-05 17:17:24 -0500

    10. Delete at index 0x1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 
 	/* if the linked list is empty -1 */
-	if (*head == NULL)
+	if (!*head)
 	{
 		return (-1);
 	}
