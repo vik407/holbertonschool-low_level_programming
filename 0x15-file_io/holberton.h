383dfe5568739d84477517c96874df61bf6506ff
Victor Hernandez <vik407@gmail.com>
2019-11-12 17:47:25 -0500

    3. cp

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -10,6 +10,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
