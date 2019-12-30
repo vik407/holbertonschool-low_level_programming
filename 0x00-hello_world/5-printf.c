d5989e1ab26a5fed09dffb7d11b1b2be4a9dd3f1
Victor Hernandez <vik407@gmail.com>
2019-12-30 18:19:11 +0000

    Weird things in old projects

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Print a string using printf
  *
  * Return: with proper grammar, but the outcome is a piece of art
  */
-
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
