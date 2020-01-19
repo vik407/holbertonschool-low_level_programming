f83e5d9d7e3e70506dfa379d04813252163a951e
Victor Hernandez <vik407@gmail.com>
2020-01-19 17:01:47 +0000

    0. >>> ht = {} 0x0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,33 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_create -
- *
- * Return: 
+ * hash_table_create - Write a function that creates a hash table.
+ * @size: The size of the array
+ * Return: a pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
+	hash_table_t *create = NULL;
 
+	if (size)
+	{
+		create = malloc(sizeof(hash_table_t));
+		if(!create)
+			return (NULL);
+
+		/*Set the list*/
+		create->size = size;
+		create->array = NULL;
+
+		create->array = malloc(sizeof(hash_node_t *) * size);
+		if (!create->array)
+			return (NULL);
+
+		/*Initialize the array for each index NULL*/
+		for (i = 0; i < size; i++)
+		{
+			create->array[i] = NULL;
+		}
+	}
+	return create;
 }
--- 0x1A-hash_tables/0-main.c
+++ 0x1A-hash_tables/0-main.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
-
 /**
  * main - check the code for Holberton School students.
  *
