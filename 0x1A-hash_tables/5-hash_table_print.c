018d7195c7e234d92ddf1f77bf5208c6fbeeedb0
Victor Hernandez <vik407@gmail.com>
2020-01-20 02:27:02 +0000

    5. >>> print(ht) 0x0

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,31 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_print -
- *
- * Return: 
+ * hash_table_print - a function that prints a hash table.
+ * @ht: is the hash table
+ * Return: a printed (printf) list
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i = 0;
+	hash_node_t *list;
+	int oddeven = 0;
 
+	if (!ht)
+		return;
+
+	printf("{");
+	while (i < ht->size)
+	{
+		list = ht->array[i];
+		while (list)
+		{
+			if (oddeven == 1)
+				printf(", ");
+			printf("'%s': '%s'", list->key, list->value);
+			oddeven = 1;
+			list = list->next;
+		}
+		i++;
+	}
+	printf("}\n");
 }
