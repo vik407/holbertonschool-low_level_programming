0716e984c2393761bf96ad9fc9948bc2a52edc10
Victor Hernandez <vik407@gmail.com>
2020-04-17 01:42:44 +0000

    fix 100 change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,54 +14,41 @@
  * followed by a new line.
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int value, count;
+	int cent = 0, coin = 0;
 
-	count = 0;
-	value = atoi(argv[1]);
-
-	if (argc != 2)
+	if (argc == 2)
 	{
-		printf("Error\n");
-		return (1);
-	}
-
-	if (value < 0)
-	{
-		printf("%d\n", 0);
+		cent = atoi(argv[1]);
+		while (cent >= 25)
+		{
+			cent -= 25;
+			coin++;
+		}
+		while (cent >= 10)
+		{
+			cent -= 10;
+			coin++;
+		}
+		while (cent >= 5)
+		{
+			cent -= 5;
+			coin++;
+		}
+		while (cent >= 2)
+		{
+			cent -= 2;
+			coin++;
+		}
+		while (cent >= 1)
+		{
+			cent -= 1;
+			coin++;
+		}
+		printf("%d\n", coin);
 		return (0);
 	}
-
-	if (value % 25 >= 0)
-	{
-		count += value / 25;
-		value = value % 25;
-	}
-
-	if (value % 10 >= 0)
-	{
-		count += value / 10;
-		value = value % 10;
-	}
-
-	if (value % 5 >= 0)
-	{
-		count += value / 5;
-		value = value % 5;
-	}
-
-	if (value % 2 >= 0)
-	{
-		count += value / 2;
-		value = value % 2;
-	}
-
-	if (value % 1 >= 0)
-	{
-		count += value / 1;
-	}
-
-	printf("%d\n", count);
-	return (0);
+	printf("Error\n");
+	return (1);
 }
